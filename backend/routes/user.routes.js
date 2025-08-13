fd6a8735a47b66f288f4dbc63583cc50b5ca053b
Aradhay Jain <aradhayjain2006@gmail.com>
2025-08-14 00:24:57 +0530

    Multer

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,9 +1,17 @@
 import express from "express"
 import { registerUser,loginUser,allUsers } from "../controllers/user.controller.js";
 import { protect } from "../middlewares/authMiddleware.js";
+import upload from "../middlewares/multer.js";
 
 const router = express.Router();
-router.post("/register", registerUser);
+router.post("/register", (req, res, next) => {
+    upload.single("pic")(req, res, function (err) {
+      if (err) {
+        return res.status(400).json({ message: "File upload failed", error: err.message });
+      }
+      next();
+    });
+}, registerUser);
 router.post("/login", loginUser);
 router.get("/", protect, allUsers);
 
