561c92280844ce9c21dc0a50159509e807839a4b
aditya <adityabibhas.sahu@gmail.com>
2025-08-26 08:38:50 +0530

    Removed bert_ner on Aradhay-chan's tantrum

--- ML/app.py
+++ ML/app.py
@@ -1,8 +1,6 @@
 from flask import Flask, request, jsonify
 from pdf_utils import download_pdf_from_url, extract_text_from_pdf
 from bert_utils import predict_with_bert
-from ner_utils import extract_entities_with_ner
-from ats_score import calculate_general_ats_score
 
 app = Flask(__name__)
 
@@ -22,19 +20,9 @@ def extract_details():
 
         # Step 2: Classify with BERT
         classification_result = predict_with_bert(pdf_text)
-
-        # Step 3: Extract entities with NER
-        ner_result = extract_entities_with_ner(pdf_text)
-
-        # Step 4: ATS Scoring
-        ats_result = calculate_general_ats_score(ner_result)
-        print(ats_result,ner_result)
-
         return jsonify({
             "extracted_text": pdf_text,
             "bert_result": classification_result,
-            "ner_result": ner_result,
-            "ats_result": ats_result
         })
 
     except Exception as e:
