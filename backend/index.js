6b4eebd28c92dafb56b616f6fe33abd2b6566004
Aradhay Jain <aradhayjain2006@gmail.com>
2025-09-02 17:52:59 +0530

    Hero2

--- backend/index.js
+++ backend/index.js
@@ -37,6 +37,8 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/public', express.static('public'));
+
 
 app.use((req, res, next) => {
     req.io = getIo();
